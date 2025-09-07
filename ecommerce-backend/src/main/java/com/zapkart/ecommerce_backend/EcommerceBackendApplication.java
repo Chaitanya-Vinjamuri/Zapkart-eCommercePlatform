ce90bbe009e3cab9daad2ce1757fbcb89c37806d
Chaitanya <chaitanyavinjamuri8@gmail.com>
2025-09-07 23:17:17 +0530

    modified Jenkinsfile

--- ecommerce-backend/src/main/java/com/zapkart/ecommerce_backend/EcommerceBackendApplication.java
+++ ecommerce-backend/src/main/java/com/zapkart/ecommerce_backend/EcommerceBackendApplication.java
@@ -2,12 +2,19 @@ package com.zapkart.ecommerce_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EcommerceBackendApplication {
+public class EcommerceBackendApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EcommerceBackendApplication.class, args);
-		System.out.println("project is working successfully!!!");
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EcommerceBackendApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceBackendApplication.class, args);
+        System.out.println("project is working successfully!!!");
+    }
 }
